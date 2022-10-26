2598941f49bdebb0eebaccf0cd471eb284688429
Nikolay Spravtsev <nicksprv@icloud.com>
2022-10-27 01:44:12 +0300

    Base JSON decode added

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -8,9 +8,12 @@ func getData(request: String) {
             print("Error: \(error))")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
             guard let data = data else { return }
-            let dataAsString = String(data: data, encoding: .utf8) ?? ""
             print("Response status code: \(response.statusCode)\n")
-            print("Data: \(dataAsString)")
+
+            let dataAsString = String(data: data, encoding: .utf8) ?? ""
+            //print("Data: \(dataAsString)")
+
+            decodeJSONData(data: Data(dataAsString.utf8))
         } else {
             if let response = response {
                 print("Response: \(response)")
@@ -19,3 +22,31 @@ func getData(request: String) {
         print("--------------------")
     }.resume()
 }
+
+struct Cards: Codable {
+    let cards: [Card]
+}
+
+struct Card: Codable {
+    let name: String?
+    let manaCost: String?
+    let type: String?
+    let setName: String?
+    let power: String?
+}
+
+func decodeJSONData(data: Data) {
+    let decoder = JSONDecoder()
+
+    do {
+        let decodedData = try decoder.decode(Cards.self, from: data)
+        print(decodedData.cards[decodedData.cards.firstIndex(where: { Card in
+            Card.name == "Opt"
+        }) ?? 0])
+    } catch {
+        print("Failed to decode JSON!")
+    }
+}
+
+let request = "https://api.magicthegathering.io/v1/cards?name=Opt"
+getData(request: request)
